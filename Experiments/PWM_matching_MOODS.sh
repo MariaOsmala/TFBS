98de6b4ce7bc0b8913f0aecd7c9d02c7c6f3321e
Osmala Maria A <osmalama@lx1-304-24194.ad.helsinki.fi>
2022-06-29 12:12:50 +0300

    MOODS script

--- Experiments/PWM_matching_MOODS.sh
+++ Experiments/PWM_matching_MOODS.sh
@@ -1,11 +1,39 @@
+cd /home/osmalama/Dropbox/Taipale-lab/Experiments
+
 source activate /home/osmalama/softwares/conda_envs/MOODS
+
+
 
 export PATH=/home/osmalama/softwares/MOODS-python-1.9.4.1/scripts:$PATH
 
-M are PWM matrices
 
-S is the sequence
+../PWMs/Yin2017/pwms/Homo_sapiens/TCF7_eDBD_HMG_HT-SELEX_TCGGAC40NCCA_KS_ASATCAAAS_1_4_NO.pfm
+../PWMs/Yin2017/pwms/Homo_sapiens/TCF7_eDBD_HMG_Methyl-HT-SELEX_TTGTCT40NGCT_KS_ASATCAAAS_1_4_NO.pfm
+../PWMs/Yin2017/pwms/Homo_sapiens/TCF7_eDBD_HMG_HT-SELEX_TCGGAC40NCCA_KS_WCATCGRGRCGCTGW_2_4_NO.pfm
+../PWMs/Yin2017/pwms/Homo_sapiens/TCF7_eDBD_HMG_Methyl-HT-SELEX_TTGTCT40NGCT_KS_WCATCGRGRCGCTGW_2_4_NO.pfm
 
-n=300000 the number of best hits
+#moods-dna.py [-h] [-v]
+#[-m M [M ...]] pfm's
+#[-S M [M ...]] pwm's
+#[-s S [S ...]] sequence
+#                   [-p p] [-t T] [-B n] [-o outfile] [--sep S] [-R]
+#                   [--no-snps] [--batch] [--bg pA pC pG pT] [--ps p]
+#                   [--log-base x] [--lo-bg pA pC pG pT]
+#                   [--threshold-precision x]
 
-moods-dna.py -S M1 M2 -s S -B $n -o outfile
+
+#M are PWM matrices of size 4 x length, space separated
+
+#S is the sequence
+S=/home/osmalama/Dropbox/Taipale-lab/Genomes/genome_assemblies_genome_fasta/ncbi-genomes-2022-06-22/GCF_000001405.40_GRCh38.p14_genomic.fna
+
+n=300000 #the number of best hits
+
+outfile=../Results/MOODS/TCF7.csv
+
+moods-dna.py -S ../PWMs/Yin2017/pwms/Homo_sapiens/TCF7_eDBD_HMG_HT-SELEX_TCGGAC40NCCA_KS_ASATCAAAS_1_4_NO.pfm \
+../PWMs/Yin2017/pwms/Homo_sapiens/TCF7_eDBD_HMG_Methyl-HT-SELEX_TTGTCT40NGCT_KS_ASATCAAAS_1_4_NO.pfm \
+../PWMs/Yin2017/pwms/Homo_sapiens/TCF7_eDBD_HMG_HT-SELEX_TCGGAC40NCCA_KS_WCATCGRGRCGCTGW_2_4_NO.pfm \
+../PWMs/Yin2017/pwms/Homo_sapiens/TCF7_eDBD_HMG_Methyl-HT-SELEX_TTGTCT40NGCT_KS_WCATCGRGRCGCTGW_2_4_NO.pfm \
+ -s $S -B $n -o $outfile \
+ /
